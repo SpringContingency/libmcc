dacd0e19cad6c93213b2d16eeeff2ae6e2dd5a98
WinterSquire <135317392+WinterSquire@users.noreply.github.com>
2025-03-18 19:13:16 +0800

    cleanup

--- include/libmcc/module/halo3/tag_files/files.h
+++ include/libmcc/module/halo3/tag_files/files.h
@@ -3,6 +3,7 @@
 #include "../../../common.h"
 
 namespace libmcc::halo3 {
+    typedef char long_string[256];
     typedef FILETIME s_file_last_modification_date;
 
     struct c_basic_buffer {
@@ -17,4 +18,19 @@ namespace libmcc::halo3 {
     struct s_file_handle {
         HANDLE handle;
     };
+
+    struct file_reference_info {
+        uint32_t signature;
+        uint16_t flags;
+        uint16_t location;
+        long_string path;
+    };
+
+    struct s_file_reference : file_reference_info {
+        s_file_handle handle;
+        int position;
+        int : 32;
+    };
+
+    static_assert(sizeof(s_file_reference) == 280);
 }
